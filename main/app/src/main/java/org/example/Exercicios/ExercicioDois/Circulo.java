1f2ca3703961cde730c9d18b264eb1b75599aec2
ArthurOReis <arthuroreis2005@gmail.com>
2025-06-04 21:41:43 -0300

    Implementando classe Circulo

--- main/app/src/main/java/org/example/Exercicios/ExercicioDois/Circulo.java
+++ main/app/src/main/java/org/example/Exercicios/ExercicioDois/Circulo.java
@@ -1,5 +1,29 @@
 package org.example.Exercicios.ExercicioDois;
 
-public class Circulo {
-    
+public class Circulo extends Forma {
+    private double raio;
+
+    public Circulo(String corDeLinha, String corDePreenchimento, double raio){
+        super(corDeLinha, corDePreenchimento);
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
 }
